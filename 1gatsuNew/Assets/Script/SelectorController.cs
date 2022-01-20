6b58c95f3b668006fa3bc965c8fc8f0b93689515
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-01-20 11:05:59 +0900

    no message

---
+++ 1gatsuNew/Assets/Script/SelectorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SelectorController : MonoBehaviour
+{
+    [SerializeField] EventSystem eventSystem;
+
+    [SerializeField] Text text;
+    GameObject selectedObj;
+
+
+
+    public void UI()
+    {
+        selectedObj = eventSystem.currentSelectedGameObject.gameObject;
+        transform.position = new Vector3(transform.position.x, selectedObj.transform.position.y, transform.position.z);
+
+    }
+}
